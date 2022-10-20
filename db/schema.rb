45a7e4045a0f1bd1fc6af6595af3c21a649c5f74
Allen Mordukhaev <allenmor93@gmail.com>
2022-10-20 18:53:34 -0400

    again

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_224912) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "players", force: :cascade do |t|
-    t.float "2P"
-    t.string "2P%"
-    t.float "2PA"
-    t.float "3P"
-    t.string "3P%"
-    t.float "3PA"
-    t.float "AST"
-    t.integer "Age"
-    t.float "BLK"
-    t.float "DRB"
-    t.float "FG"
-    t.string "FG%"
-    t.float "FGA"
-    t.float "FT"
-    t.string "FT%"
-    t.float "FTA"
-    t.integer "G"
-    t.integer "GS"
-    t.float "MP"
-    t.float "ORB"
-    t.float "PF"
-    t.float "PTS"
-    t.string "Player"
-    t.string "Player-additional"
-    t.string "Pos"
-    t.integer "Rk"
-    t.float "STL"
-    t.float "TOV"
-    t.float "TRB"
-    t.string "Tm"
-    t.string "eFG%"
-    t.string "season"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
