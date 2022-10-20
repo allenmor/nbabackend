7fed6ac8afc545f5e049c7ca2855bfa66505537b
Allen Mordukhaev <allenmor93@gmail.com>
2022-10-20 19:32:22 -0400

    fixed json

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index" 
-  post '/player', to: 'players#player'
+  post '/players2021', to: 'players#player_2021'
+  post '/players2022', to: 'players#player_2022'
   get '/players', to: 'players#all_players'
 end
