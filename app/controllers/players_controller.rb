f5f05d5097c07b2b381be011aba7cfe3072e759a
Allen Mordukhaev <allenmor93@gmail.com>
2022-10-20 20:19:18 -0400

    2020

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -42,6 +42,43 @@ class PlayersController < ApplicationController
         )
         render json: player  
         end 
+    def player_2020
+        player = Player.create(
+            "2P": params["2P"],
+            "2P%": params["2P%"],
+            "2PA": params["2PA"],
+            "3P": params["3P"],
+            "3P%": params["3P%"],
+            "3PA": params["3PA"],
+            "AST": params["AST"],
+            "Age": params["Age"],
+            "BLK": params["BLK"],
+            "DRB": params["DRB"],
+            "FG": params["FG"],
+            "FG%": params["FG%"],
+            "FGA": params["FGA"],
+            "FT": params["FT"],
+            "FT%": params["FT%"],
+            "FTA": params["FTA"],
+            "G": params["G"],
+            "GS": params["GS"],
+            "MP": params["MP"],
+            "ORB": params["ORB"],
+            "PF": params["PF"],
+            "PTS": params["PTS"],
+            "Player": params["Player"],
+            "Player-additional": params["Player-additional"],
+            "Pos": params["Pos"],
+            "Rk": params["Rk"],
+            "STL": params["STL"],
+            "TOV": params["TOV"],
+            "TRB": params["TRB"],
+            "Tm": params["Tm"],
+            "eFG%": params["eFG%"],
+            "season": '2020'
+        )
+        render json: player  
+        end 
         
         def all_players 
         render json: Player.all 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index" 
   post '/players2022', to: 'players#player_2022'
+  post '/players2020', to: 'players#player_2020'
   get '/players2021', to: 'players#players_2021'
   get '/players', to: 'players#all_players'
 end
